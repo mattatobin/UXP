a40e6d96cf1642a780afbc2ab7cc81b46cf6f985
Gaming4JC <g4jc@hyperbola.info>
2020-03-22 20:17:47 -0400

    Bug 1373798 part 2. Introduce event state flags that track the state of an element's dir attribute.

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -501,6 +501,16 @@ protected:
     RemoveStatesSilently(aStates);
     NotifyStateChange(aStates);
   }
+  virtual void ToggleStates(EventStates aStates, bool aNotify)
+  {
+    NS_PRECONDITION(!aStates.HasAtLeastOneOfStates(INTRINSIC_STATES),
+                    "Should only be removing externally-managed states here");
+    mState ^= aStates;
+    if (aNotify) {
+      NotifyStateChange(aStates);
+    }
+  }
+
 public:
   virtual void UpdateEditableState(bool aNotify) override;
 
