6f5682b54515f94d6f05f540c6ceff44c97dde22
Gaming4JC <g4jc@hyperbola.info>
2020-03-25 14:05:27 -0400

    Bug 1404789: When the shadow tree distribution changes, post a restyle + reframe.

--- dom/base/ShadowRoot.cpp
+++ dom/base/ShadowRoot.cpp
@@ -252,6 +252,24 @@ ShadowRoot::RemoveDestInsertionPoint(nsIContent* aInsertionPoint,
 }
 
 void
+ShadowRoot::DistributionChanged()
+{
+  // FIXME(emilio): We could be more granular in a bunch of cases.
+  auto* host = GetHost();
+  if (!host) {
+    return;
+  }
+
+  auto* shell = OwnerDoc()->GetShell();
+  if (!shell) {
+    return;
+  }
+
+  // FIXME(emilio): Rename this to DestroyFramesForAndRestyle?
+  shell->DestroyFramesFor(host);
+}
+
+void
 ShadowRoot::DistributeSingleNode(nsIContent* aContent)
 {
   // Find the insertion point to which the content belongs.
@@ -312,6 +330,8 @@ ShadowRoot::DistributeSingleNode(nsIContent* aContent)
   if (auto* parentShadow = foundInsertionPoint->GetParent()->GetShadowRoot()) {
     parentShadow->DistributeSingleNode(aContent);
   }
+
+  DistributionChanged();
 }
 
 void
@@ -341,6 +361,8 @@ ShadowRoot::RemoveDistributedNode(nsIContent* aContent)
     if (auto* parentShadow = insertionPoint->GetParent()->GetShadowRoot()) {
       parentShadow->RemoveDistributedNode(aContent);
     }
+
+    DistributionChanged();
     return;
   }
 }
@@ -387,6 +409,8 @@ ShadowRoot::DistributeAllNodes()
   for (ShadowRoot* shadow : shadowsToUpdate) {
     shadow->DistributeAllNodes();
   }
+
+  DistributionChanged();
 }
 
 void
