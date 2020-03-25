428cbb657d8eac4556db6f9802a87b4500d6f461
Gaming4JC <g4jc@hyperbola.info>
2020-03-25 00:06:07 -0400

    Bug 1402941 - Add HTMLSlotElement (disabled for now)
    Regenerate HTML.

--- parser/html/nsHtml5ElementName.h
+++ parser/html/nsHtml5ElementName.h
@@ -584,9 +584,9 @@ class nsHtml5ElementName
     static nsHtml5ElementName* ELT_RADIALGRADIENT;
     static nsHtml5ElementName* ELT_ROOT;
     static nsHtml5ElementName* ELT_SELECT;
-    static nsHtml5ElementName* ELT_SLOT;
     static nsHtml5ElementName* ELT_SCALARPRODUCT;
     static nsHtml5ElementName* ELT_SUBSET;
+    static nsHtml5ElementName* ELT_SLOT;
     static nsHtml5ElementName* ELT_SCRIPT;
     static nsHtml5ElementName* ELT_TFOOT;
     static nsHtml5ElementName* ELT_TEXT;
